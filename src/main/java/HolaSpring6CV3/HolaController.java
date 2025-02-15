fbd50171841fd5592ab092a24b7bc1585a8f28f2
Brandonttt <brandont81470@gmail.com>
2025-02-14 18:35:24 -0600

    Initial commit

---
+++ src/main/java/HolaSpring6CV3/HolaController.java
@@ -0,0 +1,15 @@
+package HolaSpring6CV3;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/") // Define la ruta base
+public class HolaController {
+
+    @GetMapping
+    public String holaMundo() {
+        return "Hola Mundo desde Spring Boot!";
+    }
+}
