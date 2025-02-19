ef56ca16720dda7600473564f70ae116a98fd453
Brandonttt <brandont81470@gmail.com>
2025-02-18 23:38:51 -0600

    Update HolaController.java

--- src/main/java/HolaSpring6CV3/HolaController.java
+++ src/main/java/HolaSpring6CV3/HolaController.java
@@ -1,16 +1,15 @@
 package HolaSpring6CV3;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/") // Define la ruta base
+@Controller
 public class HolaController {
 
-    @GetMapping
-    public String holaMundo() {
-        return "Hola Mundo desde Spring gfdgd!";
-        
+    @GetMapping("/")
+    public String holaSpring(Model model) {
+        model.addAttribute("mensaje", "Hola Springg");
+        return "index";
     }
 }
