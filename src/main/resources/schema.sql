9bd401f63ebf6b17d137a677bbfb62c7a786792b
Brandonttt <brandont81470@gmail.com>
2025-02-20 23:54:20 -0600

    Create schema.sql

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,27 @@
+-- Active: 1740116523482@@localhost@3306@tarea2
+DROP DATABASE IF EXISTS tarea2;
+CREATE DATABASE tarea2 CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE tarea2;
+
+CREATE TABLE usuarios (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(64) NOT NULL,
+    email VARCHAR(64) NOT NULL UNIQUE,
+    password VARCHAR(128) NOT NULL
+);
+CREATE TABLE roles (
+    id BIGINT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(64) NOT NULL UNIQUE
+);
+
+CREATE TABLE usuario_roles (
+    usuario_id BIGINT,
+    rol_id BIGINT,
+    PRIMARY KEY (usuario_id, rol_id),
+    FOREIGN KEY (usuario_id) REFERENCES usuarios(id),
+    FOREIGN KEY (rol_id) REFERENCES roles(id)
+);
+
+INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN'), ('ROLE_USER');
+
+select * FROM roles
