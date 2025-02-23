894fdfacbb15be5378e616b12b5adf2a68299478
Brandonttt <brandont81470@gmail.com>
2025-02-22 21:44:59 -0600

    Create UsuarioRepository.java

---
+++ src/main/java/HolaSpring6CV3/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package HolaSpring6CV3.repository;
+
+import HolaSpring6CV3.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Usuario findByNombre(String nombre);  // EL campo es "nombre"
+}
