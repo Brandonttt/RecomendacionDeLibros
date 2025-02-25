9b96fc4de399b205df93fe3673c44c3c1e77d02d
Brandonttt <brandont81470@gmail.com>
2025-02-25 02:42:58 -0600

    Update Usuario.java

--- src/main/java/HolaSpring6CV3/entity/Usuario.java
+++ src/main/java/HolaSpring6CV3/entity/Usuario.java
@@ -1,8 +1,21 @@
 package HolaSpring6CV3.entity;
 
-import jakarta.persistence.*;
+
 import java.util.HashSet;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "usuarios")
@@ -26,6 +39,7 @@ public class Usuario {
             joinColumns = @JoinColumn(name = "usuario_id"),
             inverseJoinColumns = @JoinColumn(name = "rol_id")
     )
+    @JsonIgnore 
     private Set<Rol> roles = new HashSet<>();
 
     // Getters y setters
@@ -69,5 +83,7 @@ public class Usuario {
     public void setRoles(Set<Rol> roles) {
         this.roles = roles;
     }
+
+   
     
 }
