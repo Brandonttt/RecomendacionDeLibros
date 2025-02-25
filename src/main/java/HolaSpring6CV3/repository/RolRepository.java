a0977429931ea9096e45f7c8bc04f1cc147c9e21
Brandonttt <brandont81470@gmail.com>
2025-02-25 02:43:08 -0600

    Create RolRepository.java

---
+++ src/main/java/HolaSpring6CV3/repository/RolRepository.java
@@ -0,0 +1,13 @@
+package HolaSpring6CV3.repository;
+
+
+import HolaSpring6CV3.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RolRepository extends JpaRepository<Rol, Long> {
+    Optional<Rol> findByNombre(String nombre);
+}
