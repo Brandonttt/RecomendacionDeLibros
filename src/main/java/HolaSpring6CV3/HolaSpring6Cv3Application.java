fe1710923c89c366d224a9384b775c2542bf40de
Brandonttt <brandont81470@gmail.com>
2025-02-25 02:43:01 -0600

    Update HolaSpring6Cv3Application.java

--- src/main/java/HolaSpring6CV3/HolaSpring6Cv3Application.java
+++ src/main/java/HolaSpring6CV3/HolaSpring6Cv3Application.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+
+
 public class HolaSpring6Cv3Application {
 
 	public static void main(String[] args) {
