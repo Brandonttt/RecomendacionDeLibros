6dea2b51e36f7d11dddc60e7acaef04ae41019ea
Brandonttt <brandont81470@gmail.com>
2025-02-25 02:42:56 -0600

    Update Rol.java

--- src/main/java/HolaSpring6CV3/entity/Rol.java
+++ src/main/java/HolaSpring6CV3/entity/Rol.java
@@ -1,8 +1,17 @@
 package HolaSpring6CV3.entity;
 
-import jakarta.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
@@ -16,6 +25,7 @@ public class Rol {
     private String nombre;
 
     @ManyToMany(mappedBy = "roles")
+    @JsonBackReference 
     private Set<Usuario> usuarios = new HashSet<>();
 
     // Getters y setters
