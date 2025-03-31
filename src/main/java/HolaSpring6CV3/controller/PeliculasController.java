71effb80315163a2958a388f29c46dc632f6eb5d
Brandonttt <brandont81470@gmail.com>
2025-03-30 22:42:48 -0600

    Create PeliculasController.java

---
+++ src/main/java/HolaSpring6CV3/controller/PeliculasController.java
@@ -0,0 +1,13 @@
+package HolaSpring6CV3.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PeliculasController {
+
+    @GetMapping("/peliculas")
+    public String peliculas() {
+        return "peliculas";
+    }
+}
